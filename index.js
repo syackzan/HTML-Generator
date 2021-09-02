3fc6f33cf4e10fdd157382dc7ce61c27ac46f5d3
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-09-02 11:47:14 -0700

    updated// index.js

--- index.js
+++ index.js
@@ -5,33 +5,40 @@ inquirer
 .prompt([
  { type: `input`,
   name: `name`,
-  message: `What is your name?`,
+  message: `What is your first name?`,
 },
 { type: `input`,
-  name: `name`,
+  name: `location`,
   message: `What is your current location?`,
 },
 { type: `input`,
-  name: `name`,
+  name: `linkedIn`,
   message: `What is you LinkedIn URL?`,
 },
 { type: `input`,
-  name: `name`,
+  name: `bio`,
   message: `Can you please share a short bio of yourself?`,
 },
 
 { type: `list`,
   choices: ["coke", "pepsi", "water"],
-  name: `name`,
+  name: `drinktype`,
   message: `Do you like coke, pepsi or water?`,
 },
 
 { type: `checkbox`,
   choices: [ "morning" , "night"],
-  name: `name`,
+  name: `typeofperson`,
   message: `Do you consider yourself a morning or night person?`,
 },
-
-
-
 ])
+
+.then((data) => { 
+  const filename = `${data.name}.json`;
+
+  fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+  err ? console.log(err) : console.log('Success!')
+  );
+});
+
+
